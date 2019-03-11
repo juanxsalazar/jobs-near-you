a2d1aeed6f13f232ee0c07937e5c32aca8975402
Juan <juanxsalazar21@gmail.com>
2019-03-10 20:15:52 -0400

    added a bit more html

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,9 +7,14 @@ class HomePage extends Component {
     <h4>A simple app to show you a map of the places hiring near your!</h4>
     <input type="search" placeholder="Search by Address or Zipcode"></input>
     <button>Search</button>
-
+    <div id="map">Map Goes Here</div>
+  
+<footer> Persistency is key</footer>
     </>
   }
 }
 
 export default HomePage
+
+// googlemaps api key:
+// jobs api key: 
